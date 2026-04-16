0e0f1aec1f8998c7b0cc93addd6ef20a4e3cc37b
Earlymikoman <xboosinger@gmail.comgit config --global user.email xboosing>
2026-04-16 13:12:13 -0700

    status code was not automatic

--- FileServer/SessionHandlers.cs
+++ FileServer/SessionHandlers.cs
@@ -253,6 +253,7 @@ public class Sessions
 
                 // The POST has no response body, so we just return and the system
                 // will return a 200 OK to the caller.
+                context.Response.StatusCode = 200;
             }
             catch (UserErrorException e)
             {
